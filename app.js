505524d0e0babd5d091fcfb81251840ba77d5b22
Max Hirschhorn <maxh@caltech.edu>
2013-05-11 21:02:24 -0700

    Basic support for receiving Erlang terms from the DSMS.
    Does not do anything useful; simply outputs the result of the decoding
    to the console.

--- app.js
+++ app.js
@@ -6,6 +6,8 @@
 var express = require('express')
   , routes = require('./routes')
   , http = require('http')
+  , net = require('net')
+  , bert = require('node-bertrpc/src/bert.js')
   , path = require('path');
 
 var app = express();
@@ -32,3 +34,28 @@ app.get('/', routes.index);
 http.createServer(app).listen(app.get('port'), function() {
   console.log("Express server listening on port " + app.get('port'));
 });
+
+net.createServer(function(sock) {
+  sock.setEncoding('binary');
+
+  // We have a connection - a socket object is assigned to the
+  // connection automatically
+  console.log("CONNECTED: " + sock.remoteAddress + ":" + sock.remotePort);
+
+  // Add a 'data' event handler to this instance of socket
+  sock.on('data', function(data) {
+    try {
+      var raw = data.toString('binary')
+        , term = bert.decode(raw);
+
+      console.log(term);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+  // Add a 'close' event handler to this instance of socket
+  sock.on('close', function(data) {
+    console.log("CLOSED: " + sock.remoteAddress + " " + sock.remotePort);
+  });
+}).listen(2158);
