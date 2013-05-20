e99a18a4fa246a80501dc30f865a1194a25fa9ae
Max Hirschhorn <maxh@caltech.edu>
2013-05-19 21:54:32 -0700

    Fixed issue with absolute URL for EventSource.

--- public/javascripts/data-stream.js
+++ public/javascripts/data-stream.js
@@ -1,6 +1,6 @@
 $(function() {
   $(document).ready(function() {
-    var source = new EventSource('stream');
+    var source = new EventSource(window.location.pathname + '/stream');
 
     source.addEventListener('message', function(e) {
       var data = JSON.parse(e.data);
