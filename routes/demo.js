5e6c9ea943ee55d71e189ba53d72fa939c4f1af0
Max Hirschhorn <maxh@caltech.edu>
2013-05-19 21:48:44 -0700

    Created route for demos.
    Loads the index page when the client makes a request. Then, the DSMS
    can send a POST request to notify them with more data.

---
+++ routes/demo.js
@@ -0,0 +1,54 @@
+
+/**
+ * Module dependencies.
+ */
+
+var redis = require('heroku-redis-client')
+  , bert = require('node-bertrpc/src/bert.js');
+
+var publisher = redis.createClient();
+
+/*
+ * GET demo.
+ */
+
+exports.load = function(req, res) {
+  var demoNo = req.params.no;
+
+  res.render('index', { title: 'YoctoDB' });
+};
+
+/*
+ * POST demo.
+ */
+
+exports.update = function(req, res) {
+  var demoNo = req.params.no;
+
+  try {
+    var data = req.body.data
+      , raw = toRaw(data)
+      , term = bert.decode(raw);
+
+    publisher.publish('updates ' + demoNo, JSON.stringify(term));
+  } catch (err) {
+    console.error(err);
+  }
+
+  res.send(200);
+};
+
+/*
+ * Returns a binary-encoded string that is recognized by BERT-JS.
+ */
+function toRaw(data) {
+  var sliced = data.slice('<<'.length, -'>>'.length)
+    , split = sliced.split(',')
+    , mapped = split.map(function(num) {
+        return String.fromCharCode(parseInt(num, 10));
+      })
+
+    , joined = mapped.join('');
+
+  return joined;
+};
