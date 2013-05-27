35d5b2beb570f5187a120741fca1fd8b20a1cabf
Max Hirschhorn <maxh@caltech.edu>
2013-05-26 17:02:12 -0700

    Added flags to indicated when appliances turn on.

--- public/javascripts/moving-average.js
+++ public/javascripts/moving-average.js
@@ -11,6 +11,65 @@ $(function() {
     }
   });
 
+  var markers = [
+      { x: 40 * 60000, title: 'On', text: 'Upright Freezer' }
+    , { x: 73 * 60000, title: 'On', text: 'Fridge Freezer' }
+    , { x: 114 * 60000, title: 'On', text: 'Upright Freezer' }
+    , { x: 139 * 60000, title: 'On', text: 'Fridge Freezer' }
+    , { x: 175 * 60000, title: 'On', text: 'Upright Freezer' }
+    , { x: 228 * 60000, title: 'On', text: 'Fridge Freezer' }
+    , { x: 242 * 60000, title: 'On', text: 'Upright Freezer' }
+    , { x: 308 * 60000, title: 'On', text: 'Upright Freezer' }
+    , { x: 384 * 60000, title: 'On', text: 'Upright Freezer' }
+    , { x: 457 * 60000, title: 'On', text: 'Kettle' }
+    , { x: 461 * 60000, title: 'On', text: 'Upright Freezer' }
+    , { x: 461 * 60000, title: 'On', text: 'CD Player' }
+    , { x: 473 * 60000, title: 'On', text: 'TV #1' }
+    , { x: 484 * 60000, title: 'On', text: 'Fridge Freezer' }
+    , { x: 510 * 60000, title: 'On', text: 'Tumble Dryer' }
+    , { x: 523 * 60000, title: 'On', text: 'CD Player' }
+    , { x: 546 * 60000, title: 'On', text: 'Upright Freezer' }
+    , { x: 552 * 60000, title: 'On', text: 'Vacuum' }
+    , { x: 560 * 60000, title: 'On', text: 'Fridge Freezer' }
+    , { x: 599 * 60000, title: 'On', text: 'VCR/DVD Player' }
+    , { x: 633 * 60000, title: 'On', text: 'Fridge Freezer' }
+    , { x: 652 * 60000, title: 'On', text: 'Upright Freezer' }
+    , { x: 704 * 60000, title: 'On', text: 'Fridge Freezer' }
+    , { x: 734 * 60000, title: 'On', text: 'Upright Freezer' }
+    , { x: 783 * 60000, title: 'On', text: 'Fridge Freezer' }
+    , { x: 808 * 60000, title: 'On', text: 'Upright Freezer' }
+    , { x: 850 * 60000, title: 'On', text: 'Fridge Freezer' }
+    , { x: 871 * 60000, title: 'On', text: 'Upright Freezer' }
+    , { x: 919 * 60000, title: 'On', text: 'Fridge Freezer' }
+    , { x: 926 * 60000, title: 'On', text: 'VCR/DVD Player' }
+    , { x: 946 * 60000, title: 'On', text: 'Hi-Fi' }
+    , { x: 955 * 60000, title: 'On', text: 'CD Player' }
+    , { x: 1003 * 60000, title: 'On', text: 'VCR/DVD Player' }
+    , { x: 1004 * 60000, title: 'On', text: 'Washing Machine' }
+    , { x: 1005 * 60000, title: 'On', text: 'Upright Freezer' }
+    , { x: 1014 * 60000, title: 'On', text: 'Fridge Freezer' }
+    , { x: 1038 * 60000, title: 'On', text: 'TV #1' }
+    , { x: 1089 * 60000, title: 'On', text: 'Fridge Freezer' }
+    , { x: 1096 * 60000, title: 'On', text: 'Washing Machine' }
+    , { x: 1109 * 60000, title: 'On', text: 'VCR/DVD Player' }
+    , { x: 1109 * 60000, title: 'On', text: 'Washing Machine' }
+    , { x: 1121 * 60000, title: 'On', text: 'Hob' }
+    , { x: 1122 * 60000, title: 'On', text: 'Washing Machine' }
+    , { x: 1135 * 60000, title: 'On', text: 'Upright Freezer' }
+    , { x: 1135 * 60000, title: 'On', text: 'Washing Machine' }
+    , { x: 1172 * 60000, title: 'On', text: 'Fridge Freezer' }
+    , { x: 1185 * 60000, title: 'On', text: 'Kettle' }
+    , { x: 1186 * 60000, title: 'On', text: 'CD Player' }
+    , { x: 1208 * 60000, title: 'On', text: 'Upright Freezer' }
+    , { x: 1211 * 60000, title: 'On', text: 'VCR/DVD Player' }
+    , { x: 1227 * 60000, title: 'On', text: 'TV #1' }
+    , { x: 1244 * 60000, title: 'On', text: 'Fridge Freezer' }
+    , { x: 1255 * 60000, title: 'On', text: 'Kettle' }
+    , { x: 1293 * 60000, title: 'On', text: 'Upright Freezer' }
+    , { x: 1356 * 60000, title: 'On', text: 'Upright Freezer' }
+    , { x: 1393 * 60000, title: 'On', text: 'Fridge Freezer' }
+  ];
+
   // Create the chart
   var chart = new Highcharts.StockChart({
     chart: {
@@ -50,181 +109,25 @@ $(function() {
     }
 
   , title: {
-      text: 'Moving average'
+      text: 'Moving average of domestic electricity model'
     }
 
   , exporting: {
       enabled: true
     }
 
-  , yAxis: {
-      plotLines: [
-        {
-          value: 190
-        , color: 'purple'
-        , dashStyle: 'shortdash'
-        , width: 2
-        , label: {
-            text: 'Fridge freezer'
-          , align: 'right'
-          , style: {
-              color: 'white'
-            , fontWeight: 'bold'
-            }
-          }
-        }
-      , {
-          value: 155
-        , color: 'purple'
-        , dashStyle: 'shortdash'
-        , width: 2
-        , label: {
-            text: 'Upright freezer'
-          , align: 'right'
-          , style: {
-              color: 'white'
-            , fontWeight: 'bold'
-            }
-          }
-        }
-      , {
-          value: 1000
-        , color: 'purple'
-        , dashStyle: 'shortdash'
-        , width: 2
-        , label: {
-            text: 'Iron'
-          , align: 'right'
-          , style: {
-              color: 'white'
-            , fontWeight: 'bold'
-            }
-          }
-        }
-      , {
-          value: 2000
-        , color: 'purple'
-        , dashStyle: 'shortdash'
-        , width: 2
-        , label: {
-            text: 'Vacuum'
-          , align: 'right'
-          , style: {
-              color: 'white'
-            , fontWeight: 'bold'
-            }
-          }
-        }
-      , {
-          value: 124
-        , color: 'purple'
-        , dashStyle: 'shortdash'
-        , width: 2
-        , label: {
-            text: 'TV 1'
-          , align: 'right'
-          , style: {
-              color: 'white'
-            , fontWeight: 'bold'
-            }
-          }
-        }
-      , {
-          value: 2400
-        , color: 'purple'
-        , dashStyle: 'shortdash'
-        , width: 2
-        , label: {
-            text: 'Hob'
-          , align: 'right'
-          , style: {
-              color: 'white'
-            , fontWeight: 'bold'
-            }
-          }
-        }
-      , {
-          value: 2125
-        , color: 'purple'
-        , dashStyle: 'shortdash'
-        , width: 2
-        , label: {
-            text: 'Oven'
-          , align: 'right'
-          , style: {
-              color: 'white'
-            , fontWeight: 'bold'
-            }
-          }
-        }
-      , {
-          value: 1250
-        , color: 'purple'
-        , dashStyle: 'shortdash'
-        , width: 2
-        , label: {
-            text: 'Microwave'
-          , align: 'right'
-          , style: {
-              color: 'white'
-            , fontWeight: 'bold'
-            }
-          }
-        }
-      , {
-          value: 2500
-        , color: 'purple'
-        , dashStyle: 'shortdash'
-        , width: 2
-        , label: {
-            text: 'Tumble dryer'
-          , align: 'right'
-          , style: {
-              color: 'white'
-            , fontWeight: 'bold'
-            }
-          }
-        }
-      , {
-          value: 406
-        , color: 'purple'
-        , dashStyle: 'shortdash'
-        , width: 2
-        , label: {
-            text: 'Washing machine'
-          , align: 'right'
-          , style: {
-              color: 'white'
-            , fontWeight: 'bold'
-            }
-          }
-        }
-      ]
-    }
-
   , series: [
       {
-        name: 'Electricity usage (kWh)'
-      , type: 'areaspline'
+        name: 'Electricity usage (W)'
       , data: []
-      , gapSize: 5
       , tooltip: {
           valueDecimals: 0
         }
-
-      , fillColor: {
-          linearGradient: {
-            x1: 0
-          , y1: 0
-          , x2: 0
-          , y2: 1
-          }
-
-        , stops: [
-            [0, Highcharts.getOptions().colors[0]]
-          , [1, 'rgba(0,0,0,0)']
-          ]
-        }
+      }
+    , {
+        type: 'flags'
+      , data: []
+      , shape: 'squarepin'
       }
     ]
   });
@@ -239,6 +142,11 @@ $(function() {
         , watts = datum[2][0];
 
       chart.series[0].addPoint([time, watts], false, false, false);
+
+      while (markers.length !== 0 && time >= markers[0].x) {
+        var flag = markers.shift();
+        chart.series[1].addPoint(flag, false, false, false);
+      }
     });
 
     chart.redraw();
